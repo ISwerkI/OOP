c22f4b25c2ac34adc8b0fd517f071b872372d048
Danil <Danil@DESKTOP-99KGHPD>
2025-10-06 17:20:00 +1000

    Optimization check DONE

--- OOP/Source.cpp
+++ OOP/Source.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 using namespace std;
 
+struct Point
+{
+	double x;
+	double y;
+};
 
 void main()
 {
 	setlocale(LC_ALL, "");
-	cout << "Hello OOP!";
+	cout << "Hello OOP!"<<endl;
+	Point A;
+	A.x = 2;
+	A.y = 3;
+	cout << A.x << "\t" << A.y << endl;
 }
