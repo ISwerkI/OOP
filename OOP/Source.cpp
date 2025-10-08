61443fc1d3ad0a076d3cf9905f144329f3ae7003
Danil <Danil@DESKTOP-99KGHPD>
2025-10-08 16:56:48 +1000

    distance DONE

--- OOP/Source.cpp
+++ OOP/Source.cpp
@@ -22,6 +22,26 @@ public:
 	{
 		this->y = y;
 	}
+	double distance_x(double x,double coordinates)
+	{
+		double distance;
+		distance = coordinates + x;
+		return distance;
+	}
+	double distance_y(double y, double coordinates)
+	{
+		double distance;
+		distance = coordinates + y;
+		return distance;
+	}
+	double distance_xy(double x, double y)
+	{
+		double distance;
+		if (x < 0) x *= -1;
+		if (y < 0) y *= -1;
+		distance = x + y;
+		return distance;
+	}
 };
 
 //#define STRUCT_POINT
@@ -44,5 +64,9 @@ void main()
 	Point A;
 	A.set_x(2);
 	A.set_y(3);
+	double distance_x=A.distance_x(5, 0);
+	double distance_y = A.distance_y(5, 0);
+	double distance_xy = A.distance_xy(5, -5);
 	cout << A.get_x() << "\t" << A.get_y()<< endl;
+	cout << distance_x << "\t" << distance_y <<"\t"<<distance_xy<< endl;
 }
